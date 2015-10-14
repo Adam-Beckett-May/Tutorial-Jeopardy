c62381e015130f1e14b8a1dfaf16f401d5a8f1e7
Adam Beckett May <adam.may@uoit.net>
2015-10-13 23:17:10 -0400

    changes made to jeopardy.c and players.c

--- jeopardy.c
+++ jeopardy.c
@@ -1,7 +1,7 @@
 /*
  * Tutorial 4 Jeopardy Project for SOFE 3950U / CSCI 3020U: Operating Systems
  *
- * Copyright (C) 2015, <GROUP MEMBERS>
+ * Copyright (C) 2015, <Adam Beckett May (100487239), Daniel Ola (100523811), Robert Kocovski (100536625), Andrew Lau (100522457)>
  * All rights reserved.
  *
  */
@@ -24,13 +24,16 @@ void tokenize(char *input, char **tokens);
 
 // Displays the game results for each player, their name and final score, ranked from first to last place
 void show_results(player *players, int num_players);
-
+{
+ 	for (int i = 0; i < 4; i++) {
+		printf("%s  %d", players[i].name, players[i].score);
+}
 
 int main(int argc, char *argv[])
 {
     // An array of 4 players, may need to be a pointer if you want it set dynamically
     player players[NUM_PLAYERS];
-    
+
     // Input buffer and and commands
     char buffer[BUFFER_LEN] = { 0 };
 
@@ -38,17 +41,26 @@ int main(int argc, char *argv[])
     initialize_game();
 
     // Prompt for players names
-    
+    for (int i=0; i < NUM_PLAYERS; i++)
+    {
+		printf("Enter name of player %d", i+1);
+		scanf("%s", &player[i].name);
+		}
     // initialize each of the players in the array
-
+    for (int i=0; i < NUM_PLAYERS; i++) {
+    		printf("Enter name of player %d", i+1);
+    		scanf("%s", &player[i].name);
+    		}
     // Perform an infinite loop getting command input from users until game ends
     while (fgets(buffer, BUFFER_LEN, stdin) != NULL)
     {
         // Call functions from the questions and players source files
-
+        display_categories();
+        	display_questions(category, value);
         // Execute the game until all questions are answered
 
         // Display the final results and exit
+        show_results(players, NUM_PLAYERS);
     }
     return EXIT_SUCCESS;
 }
--- players.c
+++ players.c
@@ -1,7 +1,7 @@
 /*
  * Tutorial 4 Jeopardy Project for SOFE 3950U / CSCI 3020U: Operating Systems
  *
- * Copyright (C) 2015, <GROUP MEMBERS>
+ * Copyright (C) 2015, <Adam Beckett May (100487239), Daniel Ola (100523811), Robert Kocovski (100536625), Andrew Lau (100522457)>
  * All rights reserved.
  *
  */
@@ -13,12 +13,28 @@
 // Returns true if the player name matches one of the existing players
 bool player_exists(player *players, int num_players, char *name)
 {
+  // Loop for number of players
+for (int i = 0, i < 4, i++)
+  // If match return true
+  if players.name[i] == name
+  {
+    return true;
+  // If none match return false
+  }
+  else
+  {
     return false;
+  }
 }
 
-// Go through the list of players and update the score for the 
+// Go through the list of players and update the score for the
 // player given their name
 void update_score(player *players, int num_players, char *name, int score)
 {
-    
+  // Loop for number of players
+for (int i = 0, i < 4 , i++)
+{
+// Set score to current player
+players(i).score = score;
+}
 }
